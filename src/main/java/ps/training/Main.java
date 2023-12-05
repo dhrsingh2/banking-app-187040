1fb14765238500dae2bbb26479ff33545299ba7a
dhrsingh2 <dhruv.singh@publicissapient.com>
2023-12-05 16:42:46 +0530

    Changing things

--- src/main/java/ps/training/Main.java
+++ src/main/java/ps/training/Main.java
@@ -2,6 +2,6 @@ package ps.training;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to PS Training!");
     }
 }
